b05c07af1be927203794d8eda4fd7dcd495365f0
Abhilash <abhilashbabuj@gmail.com>
2021-01-16 23:48:05 +0100

    problem 136

---
+++ 00136/single_number.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
+
+
+s = Solution()
+print(s.singleNumber([2, 2, 1]))
+print(s.singleNumber([4, 1, 2, 1, 2]))
